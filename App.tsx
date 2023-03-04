7306589b990ae7fd2f1c26adb0bf5161513dec74
Luis Fernando Salas Gave <lsalagav@emeal.nttdata.com>
2023-03-04 16:35:16 -0500

    Add TodoApp

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import TodoInput from './src/components/TodoInput';
+import {Provider} from 'react-redux';
+import {store} from './src/store';
+import TodoList from './src/components/TodoList';
+import Toast from 'react-native-toast-message';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Toast
+        ref={ref => {
+          Toast.setRef(ref);
+        }}
+      />
+      <TodoInput />
+      <TodoList />
+    </Provider>
+  );
+};
+
+export default App;
